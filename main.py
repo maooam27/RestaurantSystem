b802b327de4d418bbd3eac6459092b2b96f0230d
maooam27 <mfede478@gmail.com>
2024-07-25 00:23:21 +0200

    fixing databases

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
 from tkinter import *
+import sqlite3
+import tkinter.messagebox as messagebox
 
 root = Tk()
 root.title("Cassa")
@@ -13,7 +15,16 @@ def update_time():
 
 
 def SelectProduct(product):
-    print(product)
+    if (operator_name.get() == "" or n_table.get() == "") or (" " in operator_name.get() or " " in n_table.get()):
+        print("Fill operator code and table number")
+        return messagebox.showinfo("Error", "Fill operator code and table number")
+    database = sqlite3.connect("CurrentDay.db")
+    cursor = database.cursor()
+    cursor.execute(f"INSERT INTO orders VALUES (NULL, {n_table.get()}, {time.strftime('%H:%M:%S')}, "
+                   f"{operator_name.get()}, {product}")
+    print(n_table.get(), time.strftime('%H:%M:%S'), operator_name.get(), product)
+    database.commit()
+    database.close()
 
 
 # Operator name
@@ -28,24 +39,33 @@ operator_name.pack(side=LEFT)
 clock_label = Label(op_zone, text="00:00:00")
 clock_label.pack(side=RIGHT, anchor=E, padx=10)
 
+n_table = Entry(op_zone, width=3)
+n_table.pack(side=RIGHT, padx=10)
+
+table_label = Label(op_zone, text="Table:")
+table_label.pack(side=RIGHT, padx=10)
+
 # Product list
 product_zone = Frame(root, width=800, height=400)
 product_zone.pack()
 
 lambrusco = Button(product_zone, text="Lambrusco", width=10, height=5, command=lambda: SelectProduct("Lambrusco"))
-lambrusco.grid(row=0, column=0, padx=15, pady=10)
+lambrusco.grid(row=0, column=0, padx=3, pady=10)
 
 rosso = Button(product_zone, text="Rosso", width=10, height=5, command=lambda: SelectProduct("Rosso"))
-rosso.grid(row=0, column=1, padx=15, pady=10)
+rosso.grid(row=0, column=1, padx=3, pady=10)
 
 bianco = Button(product_zone, text="Bianco", width=10, height=5, command=lambda: SelectProduct("Bianco"))
-bianco.grid(row=0, column=2, padx=15, pady=10)
+bianco.grid(row=0, column=2, padx=3, pady=10)
 
 bollicine = Button(product_zone, text="Bollicine", width=10, height=5, command=lambda: SelectProduct("Bollicine"))
-bollicine.grid(row=0, column=3, padx=15, pady=10)
+bollicine.grid(row=0, column=3, padx=3, pady=10)
 
 birra = Button(product_zone, text="Birra", width=10, height=5, command=lambda: SelectProduct("Birra"))
-birra.grid(row=0, column=4, padx=15, pady=10)
+birra.grid(row=0, column=4, padx=3, pady=10)
+
+fanta = Button(product_zone, text="Fanta", width=10, height=5, command=lambda: SelectProduct("Fanta"))
+fanta.grid(row=0, column=5, padx=3, pady=10)
 
 if __name__ == "__main__":
     update_time()
